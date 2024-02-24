c1b8358c421f881886661ed5209e1670eb34f101
aholychild <159230188+aholychild@users.noreply.github.com>
2024-02-24 11:51:09 +0900

    mdn.jsを修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,4 @@
-const date = new Date();
-const [month, day, year] = [
-    date.getFullYear(),
-    date.getMonth(),
-    date.getDate(),
-];
-    console.log(date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日');
+const today = new Date().toLocaleDateString('ja-JP', {
+    dateStyle: 'long'
+});
+    console.log(today);
